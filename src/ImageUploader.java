e5989c9f47d2bbdc513afc0f188e61dc0e53edfb
stpherring <stpherring@aol.com>
2013-02-24 17:16:22 -0600

    Update src/ImageUploader.java

--- src/ImageUploader.java
+++ src/ImageUploader.java
@@ -10,18 +10,33 @@ import java.nio.file.DirectoryStream;
  * This will upload every image in the "pics" folder
  */
 
-public class ImageUploader {
-	public static void main(String[] arg){
+public class ImageUploader 
+{
+	public static void main(String[] arg)
+	{
 		//grab more files than one
 		File file = new File("pics/");
 		File[] children = file.listFiles();
 
 		ImageUpload uploader = new ImageUploadFactory().getInstance(MediaProvider.TWITTER);
-		try {
-			for (File child : children){
-				uploader.upload(child);
+		try
+		{
+			for (File child : children)
+			{
+				// All I had to do to get files into the subdirectory was rename their paths
+				
+				// Check if the file is not the subdirectory
+				if(child.isFile())
+				{
+					System.out.println(child.getName());
+					uploader.upload(child);
+					// Change path
+					child.renameTo(new File("pics/uploaded/" + child.getName()));
+				}
 			}
-		} catch (TwitterException e) {
+		} 
+		catch (TwitterException e) 
+		{
 			e.printStackTrace();
 		}
 	}
